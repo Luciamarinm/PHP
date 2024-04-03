cc6f14ccff3b64631db3ed8253976a0fd227966f
Luciamarinm <luciamarinmartinez94@gmail.com>
2024-04-03 18:33:41 +0200

    Operadores

---
+++ Ejemplo02Operadores.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $numero1 = 55;
+    $numero2 = 33;
+
+    //Operadores aritméticos
+    echo 'Suma: '.($numero1+$numero2).'<br>';
+    echo 'Resta: '.($numero1-$numero2).'<br>';
+    echo 'Multiplicación: '.($numero1*$numero2).'<br>';
+    echo 'División: '.($numero1/$numero2).'<br>';
+    echo 'Resto: '.($numero1%$numero2).'<br>';
+
+    //Operadores de incremento y decremento
+    echo "Incremento <br>";
+    echo $numero1.'<br>';
+    $numero1++;
+    echo $numero1.'<br>';
+    $numero1++;
+    echo $numero1.'<br>';
+    echo "Decremento <br>";
+    echo $numero1.'<br>';
+    $numero1--;
+    echo $numero1.'<br>';
+    $numero1--;
+    echo $numero1.'<br>';
+
+    //Operadores de asignación
+    $edad = 29;
+    echo $edad."<br>";
+    echo ($edad+=5).'<br>';
+    echo ($edad-=5).'<br>';
+    echo ($edad*=5).'<br>';
+    echo ($edad/=5).'<br>';
+    echo ($edad%=5).'<br>';
+
+    ?>
+</body>
+</html>
