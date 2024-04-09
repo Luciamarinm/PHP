5a59be5ad3383c5cb7a3f2fc1312e7f91ddacee5
Luciamarinm <luciamarinmartinez94@gmail.com>
2024-04-09 18:19:46 +0200

    Bucle for

---
+++ conceptos-basicos/01-hola-mundo/Ejemplo06BucleFor.php
@@ -0,0 +1,24 @@
+<?php
+
+    //For
+    $suma = 0;
+    for($i=0 ; $i <=10 ;$i++) {
+        $suma += $i;
+        echo $suma."<br>";
+    }
+
+    //isset es para comprobar si una variable existe
+    if(isset($_GET['numero'])) {
+        //Cambiar tipo de dato
+        $numero = (int)$_GET['numero'];
+    } else {
+        $numero = 1;
+    }
+
+    echo "<h1>Tabla de multiplicar del número $numero</h1>";
+
+    for($contador = 1 ; $contador <= 10 ; $contador++) {
+        echo "$numero x $contador = ".($numero*$contador)."<br>";
+    }
+
+?>
